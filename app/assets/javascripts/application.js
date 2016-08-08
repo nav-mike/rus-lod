49bcd3bfaf69972d05dc5279cd589fdf1320e224
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-08-08 15:52:27 +0300

    add test editor view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,22 +1,29 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
-// // listed below.
-// //
-// // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-// //
-// // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// // compiled file.
-// //
-// // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// // about supported directives.
-// //
-// //= require jquery
-// //= require jquery_ujs
-// //= require bootbox
-// //= require bootstrap-sprockets
-// //= require_tree .
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require ace/ace
+//= require ace/theme-textmate
+//= require ace/theme-github
+//= require ace/mode-sql
+//= require jquery-ace.min
+//= require bootbox
+//= require bootstrap-sprockets
+//= require_tree .
 
 $(document).ready(function() {
   $.material.init();
+
+   $('.my-code-area').ace({ theme: 'github', lang: 'sql' })
 });
 
--- app/controllers/mappings_controller.rb
+++ app/controllers/mappings_controller.rb
@@ -1,4 +1,15 @@
 class MappingsController < ApplicationController
   def index
+    @sparql = "PREFIX ifmolod:<http://lod.ifmo.ru/>
+    CREATE VIEW Staff AS CONSTRUCT {
+    ?person a foaf:Person ;
+    vivoplus:pcardId ?pcardId;
+    foaf:firstName ?firstName;
+    vivo:middleName ?middleName;
+    foaf:lastName ?lastName;
+    rdfs:label ?label;
+    vivo:affiliatedOrganization ?laboratory;
+    a ?supervisorType.
+    }"
   end
 end
