6a4eb966c5f8f367663661e316e4ed1708e72830
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-08-08 15:02:50 +0300

    add fake data to mappings index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'mappings#index'
 
+  get '/mappings' => 'mappings#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
