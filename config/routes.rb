81988a1db9f8f7f2032c84721fb0d03b27772c88
Mikhail Navrotskiy <m.navrotskiy@hotmail.com>
2016-08-08 16:46:58 +0000

    add data index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'data/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
 
   get '/mappings' => 'mappings#index'
   get '/mappings/new' => 'mappings#new'
+  get '/data' => 'data#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
