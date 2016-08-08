0ab6f249b0fa189a14edfeaa558480f5dcf634a9
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-08-08 14:14:32 +0300

    add mappings controller

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,14 @@ module RusLod
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.orm :active_record
+      g.template_engine :slim
+      g.test_framework false
+      g.jbuilder false
+      g.assets false
+      g.helper false
+    end
   end
 end
