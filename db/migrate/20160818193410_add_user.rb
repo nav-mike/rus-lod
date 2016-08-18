34f179ccd9793d9873305ec51f3e195ef644fb52
Mikhail Navrotskiy <m.navrotskiy@hotmail.com>
2016-08-18 19:42:53 +0000

    fix sign in page

---
+++ db/migrate/20160818193410_add_user.rb
@@ -0,0 +1,9 @@
+class AddUser < ActiveRecord::Migration
+  def up
+    User.create email: 'm.navrotskiy@gmail.com', password: '12345678'
+  end
+  
+  def down
+    User.where(email: 'm.navrotskiy@gmail.com').delete_all
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818185919) do
+ActiveRecord::Schema.define(version: 20160818193410) do
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
