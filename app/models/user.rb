06e21b3c937f402660458c29c244046fc8939245
Mikhail Navrotskiy <m.navrotskiy@hotmail.com>
2016-08-18 19:12:56 +0000

    config rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   rolify
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
+
+  default_scope { order(:email) }
 end
