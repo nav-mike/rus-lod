e392fc3e338cb48122e4c46ec6cc6ee8b7058905
Mikhail Navrotskiy <m.navrotskiy@hotmail.com>
2016-10-12 07:32:49 +0000

    #6 add rspec config files

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+require 'simplecov'
+require 'capybara/rspec'
+require 'rspec/json_expectations'
+require 'webmock/rspec'
+require 'faker'
+SimpleCov.start 'rails'
+
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color = true
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+  # Use the specified formatter
+  config.formatter = :documentation
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  # use mocha
+  config.mock_with :mocha
+end
